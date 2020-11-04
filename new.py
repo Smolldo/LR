5c138ce4516d6e663ecf9957c550ff724d27699b
Smolldo <maximqwerbich@gmail.com>
2020-11-04 13:50:44 +0200

    add new func

--- new.py
+++ new.py
@@ -1,5 +1,13 @@
 def func(a, b = 5, c = 10):
 	print('a = ',a, 'b = ',b, 'c = ',c)
+
+def maximum(x, y):
+	if x > y:
+		return x
+	elif x == y:
+		return "They r equal"
+	else:
+		return y
+print(maximum(2, 3))
+
 func(3, 7)
-func(25, c = 24)
-func(c = 10, a = 100)
